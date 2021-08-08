4588f4fadc19df9edd4fa900bd1a6ec568cb71d8
Eternity <66208385+EternityGT@users.noreply.github.com>
2021-08-08 07:25:10 +0700

    Added
    Made By ISLA

---
+++ index.js
@@ -0,0 +1,65 @@
+"use-strict";
+
+//Library
+const express = require("express");
+const nhentai = require("./nhentai");
+const app = express();
+const path = require("path");
+
+//Constant Variables & Global Variables
+const PORT = process.env.PORT || 3000;
+const ROOT = path.join(__dirname, "public", "html");
+
+//Middleware
+app.set("view engine", "ejs");
+app.set("views", ROOT);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.all("/public/*", (req, res) => {
+    res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/download", checkId, async (req, res) => {
+    const result = req.nhentai;
+    res.render("download", { name: result.title.pretty, id: result.id, image: result.thumbnail_data });
+});
+
+app.post("/download/isla", checkData, (req, res, next) => {
+    res.set("Content-Disposition", "attachment; filename=" + req.nhentai.id + ".zip");
+    res.set("Content-Type", "file/zip");
+    let buffer = req.nhentai.buffer;
+    res.status(200).send(buffer);
+});
+
+app.use((req, res) => {
+    res.redirect("/");
+});
+
+app.listen(PORT, () => {
+    console.log("App listening on port " + PORT);
+});
+
+//Custom Middleware
+async function checkId(req, res, next) {
+    let isValid = await nhentai.test(req.body.id).catch(() => null);
+    if (isValid) {
+        req.nhentai = isValid;
+        return next();
+    } else res.status(404).render("notFound", { id: req.body.id });
+}
+
+async function checkData(req, res, next) {
+    const { id } = req.body;
+    try {
+        let res = await nhentai.download(id);
+        req.nhentai = { id: res.id, buffer: res.buffer };
+        return next();
+    } catch (err) {
+        res.redirect("/");
+    }
+}
---
+++ public/script/download.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const button = document.querySelector("[download-btn]");
+// button.addEventListener("click", () => {
+//     axios.post("/download/isla", {
+//         id: doujinId,
+//     });
+// });
